d38bce09de8eb083beca469fe88bcacc5d274070
Sadman Shawmik <hello@sadmansh.com>
2023-07-15 12:28:09 -0400

    Create JobPost and other associated models

---
+++ backend/app/models/user.py
@@ -0,0 +1,25 @@
+"""
+Model: User
+"""
+
+from extensions import db
+from sqlalchemy import Column, String, func
+from sqlalchemy.types import Integer, DateTime, Text, Enum
+import enum
+
+class User(db.Model):
+	__tablename__ = 'user'
+	
+	id = Column(Integer, primary_key=True)
+	email = Column(String(100), nullable=False)
+	password = Column(String(100), nullable=False)
+	oauth_token = Column(String(255), nullable=False)
+	oauth_token_secret = Column(String(255), nullable=False)
+	created_at = Column(DateTime, nullable=False, default=func.now())
+	updated_at = Column(DateTime, nullable=False, default=func.now(), onupdate=func.now())
+	type = Column(String(50))
+	
+	__mapper_args__ = {
+		'polymorphic_identity': 'user',
+		'polymorphic_on': type
+	}
