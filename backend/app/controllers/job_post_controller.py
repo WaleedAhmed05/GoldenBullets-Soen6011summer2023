3aff7e999c2d96a27ec9ed43c5524bb4104b9bd8
Sadman Shawmik <hello@sadmansh.com>
2023-07-15 13:02:19 -0400

    Create endpoint for individual job posts

--- backend/app/controllers/job_post_controller.py
+++ backend/app/controllers/job_post_controller.py
@@ -7,3 +7,9 @@ class JobPostController:
 			return [job_post.serialize() for job_post in job_posts]
 		except Exception as e:
 			return {'error': str(e)}, 500
+		
+	def get_job_post(id):
+		try:
+			return JobPostService.get_job_post(id)
+		except Exception as e:
+			return {'error': str(e)}, 500
--- backend/app/services/job_post_service.py
+++ backend/app/services/job_post_service.py
@@ -4,3 +4,7 @@ class JobPostService:
     @staticmethod
     def get_job_posts():
         return JobPost.query.all()
+    
+    @staticmethod
+    def get_job_post(id):
+        return JobPost.query.get(id).serialize()
