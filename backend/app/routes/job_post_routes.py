902f291507b04ef17ee9a31f126b7b4947f1ed21
Sadman Shawmik <hello@sadmansh.com>
2023-07-15 13:02:19 -0400

    Create endpoint for individual job posts

--- backend/app/routes/job_post_routes.py
+++ backend/app/routes/job_post_routes.py
@@ -10,3 +10,11 @@ def get_job_posts():
 		return jsonify(job_posts)
 	except Exception as e:
 		return jsonify({'error': str(e)}), 500
+	
+@job_post_routes.route('/<int:id>', methods=['GET'])
+def get_job_post(id):
+	try:
+		job_post = JobPostController.get_job_post(id)
+		return jsonify(job_post)
+	except Exception as e:
+		return jsonify({'error': str(e)}), 500
