521fb858f97597e5a0fe759cfab3bf6bff616811
Sadman Shawmik <hello@sadmansh.com>
2023-07-15 20:16:05 -0400

    Add user data route

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -3,6 +3,7 @@ Handles Flask Dance routes for Google OAuth authentication
 """
 from flask import Blueprint
 from controllers.auth import AuthController
+from flask_jwt_extended import jwt_required
 
 auth_routes = Blueprint('auth_routes', __name__, url_prefix='/auth')
 
@@ -10,3 +11,10 @@ auth_routes = Blueprint('auth_routes', __name__, url_prefix='/auth')
 @auth_routes.route('/login')
 def google_login():
 	return AuthController.google_login()
+
+# Return user data
+@auth_routes.route('/user')
+@jwt_required()
+def user():
+	print('Request to /user')
+	return AuthController.user_data()
