df5c758924ebe017e0ac01c5a981bc2eb5f04346
Sadman Shawmik <hello@sadmansh.com>
2023-07-18 22:43:18 -0400

    Refactor & combine backend code

--- backend/app/services/auth.py
+++ backend/app/services/auth.py
@@ -4,50 +4,63 @@ from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity
 from os import getenv
 from extensions import db
 from models.user import User
+from models.employer import Employer
 
 class AuthService:
 	@staticmethod
 	def google_login():
-		if not google.authorized:
-			return url_for('google.login')
+		try:
+			if not google.authorized:
+				return url_for('google.login')
 
-		account_info = google.get('/oauth2/v2/userinfo')
-		if account_info.ok:
-			account_info_json = account_info.json()
-			email = account_info_json['email']
-			# Check if user is in database
-			user = User.query.filter_by(email=email).first()
-			if not user:
-				# Create user 
-				name = account_info_json['name']
-				# Split name into first and last name
-				name_split = name.split(' ')
-				first_name = name_split[0]
-				last_name = name_split[-1]
-				user = User(email=email, 
-				first_name=first_name, last_name=last_name)
-				db.session.add(user)
-				db.session.commit()
-			# Create JWT token
-			access_token = create_access_token(identity=email)
-			# Return url to frontend with JWT token
-			redirect_url = request.args.get('redirect_url') or getenv('FRONTEND_URL')
-			return f'{redirect_url}?token={access_token}'
-		else:
-			return {'error': 'Failed to fetch user info'}
+			account_info = google.get('/oauth2/v2/userinfo')
+			if account_info.ok:
+				account_info_json = account_info.json()
+				email = account_info_json['email']
+				# Check if user is in database
+				user = User.query.filter_by(email=email).first()
+				if not user:
+					# Create user 
+					name = account_info_json['name']
+					# Split name into first and last name
+					name_split = name.split(' ')
+					first_name = name_split[0]
+					last_name = name_split[-1]
+					user = User(email=email, 
+					first_name=first_name, last_name=last_name)
+					db.session.add(user)
+					db.session.commit()
+				# Create JWT token
+				access_token = create_access_token(identity=email)
+				# Return url to frontend with JWT token
+				redirect_url = request.args.get('redirect_url') or getenv('FRONTEND_URL')
+				return f'{redirect_url}?token={access_token}'
+			else:
+				return {'error': 'Failed to fetch user info'}
+		except Exception as e:
+			return {'error': str(e)}
 		
 	@staticmethod
 	def user_data():
-		user_email = get_jwt_identity()
-		user = User.query.filter_by(email=user_email).first()
-		if user:
-			return {
-				'email': user.email,
-				'first_name': user.first_name,
-				'last_name': user.last_name,
-				'type': user.type,
-			}
-		else:
-			return {'error': 'User not found'}
+		try:
+			user_email = get_jwt_identity()
+			user = User.query.filter_by(email=user_email).first()
+			if user:
+				# Get company_id from employer model if user is employer
+				if user.type == 'employer':
+					employer = Employer.query.filter_by(id=user.id).first()
+					company_id = employer.company_id
+				return {
+					'id': user.id,
+					'email': user.email,
+					'first_name': user.first_name,
+					'last_name': user.last_name,
+					'type': user.type,
+					'company_id': company_id if user.type == 'employer' else None
+				}
+			else:
+				return {'error': 'User not found'}
+		except Exception as e:
+			return {'error': str(e)}
 
 		
--- backend/app/services/job_post_service.py
+++ backend/app/services/job_post_service.py
@@ -1,4 +1,7 @@
 from models.job_post import JobPost
+from models.user import User
+from extensions import db
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
 
 class JobPostService:
     @staticmethod
@@ -7,5 +10,79 @@ class JobPostService:
         return [job_post.serialize() for job_post in job_posts]
     
     @staticmethod
+    @jwt_required()
+    def get_employer_job_posts():
+        try:
+            # Verify jwt token
+            if not get_jwt_identity():
+                return {'error': 'Unauthorized'}, 401
+            # Get user email from jwt token
+            user_email = get_jwt_identity()
+            user = User.query.filter_by(email=user_email).first()
+            # Check if user type is employer
+            if user.type != 'employer':
+                return {'error': 'Unauthorized'}, 401
+            
+            job_posts = JobPost.query.filter_by(employer_id=user.id).all()
+            return [job_post.serialize() for job_post in job_posts]
+        except Exception as e:
+            return {'error': str(e)}, 500
+    
+    @staticmethod
     def get_job_post(id):
         return JobPost.query.get(id).serialize()
+    
+    @staticmethod
+    @jwt_required()
+    def create_job_post(request):
+        try:
+            # Verify jwt token
+            if not get_jwt_identity():
+                return {'error': 'Unauthorized'}, 401
+            # Get user email from jwt token
+            user_email = get_jwt_identity()
+            user = User.query.filter_by(email=user_email).first()
+            # Check if user type is employer
+            if user.type != 'employer':
+                return {'error': 'Unauthorized'}, 401
+            
+            title = request.json['title']
+            description = request.json['description']
+            location = request.json['location']
+            salary = request.json['salary']
+            job_type = request.json['job_type']
+            employer_id = user.id
+            company_id = request.json['company_id']
+            job_post = JobPost(title=title, description=description, location=location, salary=salary, job_type=job_type, employer_id=employer_id, company_id=company_id)
+            db.session.add(job_post)
+            db.session.commit()
+
+            return job_post.serialize()
+        except Exception as e:
+            return {'error': str(e)}, 400
+    
+    @staticmethod
+    @jwt_required()
+    def update_job_post(id, request):
+        try:
+            # Verify jwt token
+            if not get_jwt_identity():
+                return {'error': 'Unauthorized'}, 401
+            
+            job_post = JobPost.query.get(id)
+            # Check if user is the owner of the job post
+            user_email = get_jwt_identity()
+            user = User.query.filter_by(email=user_email).first()
+            if user.id != job_post.employer_id:
+                return {'error': 'Unauthorized'}, 401
+            
+            job_post.title = request.json['title']
+            job_post.description = request.json['description']
+            job_post.location = request.json['location']
+            job_post.salary = request.json['salary']
+            job_post.job_type = request.json['job_type']
+
+            db.session.commit()
+            return job_post.serialize()
+        except Exception as e:
+            return {'error': str(e)}, 400
