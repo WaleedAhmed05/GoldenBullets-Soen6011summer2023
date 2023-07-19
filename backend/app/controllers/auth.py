d8bf1b077ba43339687161cf9ca128ee5e50f5d4
Sadman Shawmik <hello@sadmansh.com>
2023-07-19 00:38:47 -0400

    Fix candidate/employer differentiation

--- backend/app/controllers/auth.py
+++ backend/app/controllers/auth.py
@@ -1,11 +1,19 @@
 from flask import redirect, jsonify
 from services.auth import AuthService
 
-class AuthController:		
+class AuthController:
 	def google_login():
 		try:
+			# Redirect to google login
+			url = AuthService.google_login()
+			return redirect(url)
+		except Exception as e:
+			return {'error': str(e)}, 500
+
+	def google_login_callback():
+		try:
 			# Return authservice redirect if it is a url
-			response = AuthService.google_login()
+			response = AuthService.google_login_callback()
 			# If response is string, redirect. Else return response
 			if isinstance(response, str):
 				return redirect(response)
--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -9,13 +9,17 @@ from os import getenv
 auth_routes = Blueprint('auth_routes', __name__, url_prefix='/auth')
 
 # Google login route
-@auth_routes.route('/login/callback')
+@auth_routes.route('/login')
 def google_login():
 	return AuthController.google_login()
+
+# Google login callback route
+@auth_routes.route('/login/callback')
+def google_login_callback():
+	return AuthController.google_login_callback()
 
 # Return user data
 @auth_routes.route('/user')
 @jwt_required()
 def user():
-	print('Request to /user')
 	return AuthController.user_data()
