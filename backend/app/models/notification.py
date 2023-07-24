68645e5c25d985e2f5b2284f534e500e67eabff6
Sadman Shawmik <hello@sadmansh.com>
2023-07-24 16:22:38 -0400

    Add notification status enum

--- backend/app/models/notification.py
+++ backend/app/models/notification.py
@@ -4,8 +4,12 @@ Model: Notification
 
 from extensions import db
 from sqlalchemy import Column, String, func, ForeignKey
-from sqlalchemy.types import Integer, DateTime, Text
+from sqlalchemy.types import Integer, DateTime, Text, Enum
+import enum
 
+class NotificationStatusEnum(enum.Enum):
+	READ = 'read'
+	UNREAD = 'unread'
 class Notification(db.Model):
 	__tablename__ = 'notification'
 	
@@ -13,6 +17,7 @@ class Notification(db.Model):
 	user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
 	title = Column(String(255), nullable=False)
 	body = Column(Text, nullable=True)
+	status = Column(Enum(NotificationStatusEnum), nullable=False, default=NotificationStatusEnum.UNREAD)
 	created_at = Column(DateTime, nullable=False, server_default=func.now())
 	
 	def serialize(self):
@@ -21,5 +26,6 @@ class Notification(db.Model):
 			'user_id': self.user_id,
 			'title': self.title,
 			'body': self.body,
+			'status': self.status.value,
 			'created_at': self.created_at,
 		}
