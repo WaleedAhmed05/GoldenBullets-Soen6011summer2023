927c0830576516eb68cfbbcb5539eff314a06dee
SimonSaid1996 <SimonSaid1996@gmail.com>
2023-07-31 10:20:30 -0400

    fixed code of viewing potential employers

--- backend/app/controllers/CandidateController.py
+++ backend/app/controllers/CandidateController.py
@@ -15,3 +15,10 @@ class CandidateController:
             return profile
         except Exception as e:
             return {'error': str(e)}, 500
+
+    def get_employers(id):
+        try:
+            employers = jsonify(CandidateService.get_potential_employer(id))
+            return employers
+        except Exception as e:
+            return {'error': str(e)}, 500
