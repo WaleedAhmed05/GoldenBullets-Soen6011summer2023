5b449a0db8b0d2aa983d5c72931fb1c1adbcd74e
Sadman Shawmik <hello@sadmansh.com>
2023-07-31 19:55:48 -0400

    Fix user profile backend handler errors

--- backend/app/controllers/candidate.py
+++ backend/app/controllers/candidate.py
@@ -1,24 +1,18 @@
 from services.candidate import CandidateService
-from flask import jsonify
+from flask import jsonify, request
 
 class CandidateController:
-    def update_profile():
+    def update_profile(id, request):
         try:
-            return CandidateService.update_profile()
+            return CandidateService.update_profile(id, request)
         except Exception as e:
+            print('Error: ', e)
             return {'error': str(e)}, 500
-
 
     def get_profile(id):
         try:
             profile = jsonify(CandidateService.get_profile(id))
             return profile
         except Exception as e:
-            return {'error': str(e)}, 500
-
-    def get_employers(id):
-        try:
-            employers = jsonify(CandidateService.get_potential_employer(id))
-            return employers
-        except Exception as e:
+            print('Error: ', e)
             return {'error': str(e)}, 500
