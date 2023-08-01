87b1e6376b2dff9eef71c4cdb235965014ef9d89
Sadman Shawmik <hello@sadmansh.com>
2023-07-31 20:25:17 -0400

    Fix eslint error

--- frontend/components/jobs/ThankYou.js
+++ frontend/components/jobs/ThankYou.js
@@ -7,7 +7,7 @@ const ThankYou = ({ job }) => {
 			<Header />
 			<main className={styles.thankYouPage}>
 				<div className="container">
-					<h1>Your application to the job "{job?.title}" has been successful. </h1>
+					<h1>Your application to the job {job?.title} has been successful. </h1>
 					<p>You will hear back from the recruiter within 10 business days.</p>
 				</div>
 			</main>
