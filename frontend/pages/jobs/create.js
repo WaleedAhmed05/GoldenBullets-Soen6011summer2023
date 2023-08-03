67344f563cc343f5a66223e1235baa7aa9eae570
Nastaran-Naseri <49358045+Nastaran-Naseri@users.noreply.github.com>
2023-08-03 17:18:14 -0400

    update Create Jobs Form

--- frontend/pages/jobs/create.js
+++ frontend/pages/jobs/create.js
@@ -52,23 +52,23 @@ const CreateJob = () => {
 					)}
 					{user ? (
 						<>
-							<h1>Create a job</h1>
+							<h1 className={styles.CreateJobh1}>Create <span> a job</span></h1>
 							<form onSubmit={createJob} className={styles.createJobForm}>
 								<div className={styles.formGroup}>
 									<label htmlFor="title">Title</label>
-									<input type="text" id="title" name="title" required value={job.title} onChange={(e) => setJob({ ...job, title: e.target.value })} />
+									<input type="text" id="title" placeholder="Enter the job title" name="title" required value={job.title} onChange={(e) => setJob({ ...job, title: e.target.value })} />
 								</div>
 								<div className={styles.formGroup}>
 									<label htmlFor="description">Description</label>
-									<textarea id="description" name="description" required value={job.description} onChange={(e) => setJob({ ...job, description: e.target.value })} />
+									<textarea id="description" placeholder="Enter the job description" name="description" required value={job.description} onChange={(e) => setJob({ ...job, description: e.target.value })} />
 								</div>
 								<div className={styles.formGroup}>
 									<label htmlFor="location">Location</label>
-									<input type="text" id="location" name="location" required value={job.location} onChange={(e) => setJob({ ...job, location: e.target.value })} />
+									<input type="text" id="location" placeholder="Enter the job location" name="location" required value={job.location} onChange={(e) => setJob({ ...job, location: e.target.value })} />
 								</div>
 								<div className={styles.formGroup}>
 									<label htmlFor="salary">Salary</label>
-									<input type="text" id="salary" name="salary" required value={job.salary} onChange={(e) => setJob({ ...job, salary: e.target.value })} />
+									<input type="text" id="salary" placeholder="Enter the job salary" name="salary" required value={job.salary} onChange={(e) => setJob({ ...job, salary: e.target.value })} />
 								</div>
 								<div className={styles.formGroup}>
 									<label htmlFor="job_type">Job type</label>
