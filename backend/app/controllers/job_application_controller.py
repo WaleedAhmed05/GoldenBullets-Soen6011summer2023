dfab94fd010ef4dcd7d21380e95e67eb34d56838
Nastaran-Naseri <49358045+Nastaran-Naseri@users.noreply.github.com>
2023-08-03 16:00:02 -0400

    Update job_application_controller

--- backend/app/controllers/job_application_controller.py
+++ backend/app/controllers/job_application_controller.py
@@ -28,6 +28,9 @@ class JobApplicationController:
 	def update_job_post_application(application_id, request):
 		try:
 			status = request.get_json()['status']
+			if status == 'interview':
+				interview = request.get_json()['invite']
+				return JobApplicationService.set_job_application_interview(application_id, status, interview)
 			return JobApplicationService.update_job_post_application(application_id, status)
 		except Exception as e:
 			print('error', e)
@@ -36,6 +39,21 @@ class JobApplicationController:
 	def get_applications_by_candidate():
 		try:
 			return JobApplicationService.get_applications_by_candidate()
+		except Exception as e:
+			print('error', e)
+			return {'error': str(e)}, 500
+
+	def get_job_application_interview(job_id, application_id):
+		try:
+			application = jsonify(JobApplicationService.get_job_application_interview(job_id, application_id))
+			return application
+		except Exception as e:
+			return {'error': str(e)}, 500
+
+	def set_job_application_interview(application_id, request):
+		try:
+			interview = request.get_json()
+			return JobApplicationService.set_job_application_interview(application_id, interview)
 		except Exception as e:
 			print('error', e)
 			return {'error': str(e)}, 500
