9056c001533bac3491d50b80af80cf16eac67551
WaleedAhmed05 <waloo_waleed@yahoo.com>
2023-08-07 23:52:32 -0400

    Google auth callback & user route tests

--- backend/app/tests/auth_test.py
+++ backend/app/tests/auth_test.py
@@ -20,6 +20,18 @@ class TestAuthRoutes(unittest.TestCase):
         response = self.app.get('/auth/login', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
+    @patch('controllers.auth.AuthController.google_login_callback')
+    def test_google_login_callback(self, mock_google_login_callback):
+        # Mock the AuthController.google_login_callback() method
+        # to avoid any external dependencies during testing
+        mock_google_login_callback.return_value = "Google login callback response"
+        response = self.app.get('/auth/login/callback')
+        self.assertEqual(response.status_code, 200)
+
+    def test_user(self):
+        # Simulate a request to the user route
+        response = self.app.get('/auth/user')
+        self.assertEqual(response.status_code, 401)  # Assumption: Unauthorized access should return 401
 
 if __name__ == '__main__':
     unittest.main()
