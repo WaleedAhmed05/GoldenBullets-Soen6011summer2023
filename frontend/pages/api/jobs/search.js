1bba5852c1f295d0a7ee9b66e5642fa2ba8b81e9
Sadman Shawmik <hello@sadmansh.com>
2023-08-08 13:58:01 -0400

    Add search function in jobs page

---
+++ frontend/pages/api/jobs/search.js
@@ -0,0 +1,30 @@
+export default async (req, res) => {
+	try {
+		// Get access token from authorization header
+		const authHeader = req.headers.authorization
+		if (!authHeader) {
+			return res.status(401).json({ message: 'Authorization header missing' })
+		}
+		if (req.method === 'GET') {
+			// Url encode the query string
+			const query = encodeURIComponent(req.query.q)
+			// Send a GET request to the API to search jobs
+			const response = await fetch(`${process.env.API_URL}/api/jobs/search?q=${query}`, {
+				method: 'GET',
+				headers: {
+					Authorization: authHeader,
+					'Content-Type': 'application/json',
+				},
+			})
+			const data = await response.json()
+			if (response.ok) {
+				return res.status(200).json(data)
+			}
+		} else {
+			return res.status(405).json({ message: 'Method not allowed' })
+		}
+	} catch (error) {
+		console.error('Error accessing api/jobs/search', error)
+		return res.status(403).json({ message: 'User forbidden' })
+	}
+}
--- frontend/pages/jobs/index.js
+++ frontend/pages/jobs/index.js
@@ -1,14 +1,38 @@
 import Link from 'next/link'
+import { useState, useEffect } from 'react'
 import { useAppContext } from '@/context/AppContext'
 import Header from '@/components/layout/Header'
 import styles from '@/styles/Jobs.module.scss'
 import { prettifyJobType } from '@/utils'
 
-const Jobs = ({ jobs }) => {
-	const { user } = useAppContext()
-	console.log('user', user)
-	// Check if user has already applied for this job
+const Jobs = ({ data }) => {
+	const [jobs, setJobs] = useState(data)
+	const [search, setSearch] = useState('')
+	const { user, token } = useAppContext()
 
+	useEffect(() => {
+		setJobs(data)
+	}, [data])
+
+	// Check if user has already applied for this job
+	const hasApplied = (jobId) => {
+		if (!user) return false
+		return user.job_applications?.some(application => application.job_post_id === jobId)
+	}
+
+	// Search jobs
+	const searchJobs = async (e) => {
+		e.preventDefault()
+		const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/jobs/search?q=${search}`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`
+			}
+		})
+		const jobs = await res.json()
+		setJobs(jobs)
+	}
 
 	return (
 		<>
@@ -16,6 +40,13 @@ const Jobs = ({ jobs }) => {
 			<main className={styles.jobsPage}>
 				<div className="container">
 					<h1>Browse all jobs</h1>
+
+					{/* Search */}
+					<form className={styles.search} onSubmit={searchJobs}>
+						<input type="text" placeholder="Search jobs" onChange={(e) => setSearch(e.target.value)} />
+						<button type="submit">Search</button>
+					</form>
+
 					<div className={styles.jobs}>
 						{jobs?.length && jobs.map(job => (
 							<div key={job.id} className={styles.job}>
@@ -28,7 +59,11 @@ const Jobs = ({ jobs }) => {
 										<li className={styles.jobMetaItem}>{job.salary}</li>
 									</ul>
 								</Link>
-								<Link href={`/jobs/${job.id}/apply`} className={styles.jobApply}>Apply</Link>
+								{hasApplied(job.id) ?
+									<span style={{ color: '#000' }}>Applied</span>
+									:
+									<Link href={`/jobs/${job.id}/apply`} className={styles.jobApply}>Apply</Link>
+								}
 							</div>
 						))}
 					</div>
@@ -44,7 +79,7 @@ export async function getServerSideProps() {
 
 	return {
 		props: {
-			jobs,
+			data: jobs,
 		},
 	}
 }
