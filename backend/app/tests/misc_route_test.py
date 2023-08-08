da00194d0c1398bb64f25c2ab051114f60568bd5
WaleedAhmed05 <waloo_waleed@yahoo.com>
2023-08-08 02:00:01 -0400

    More tests for mini features like job filteration

---
+++ backend/app/tests/misc_route_test.py
@@ -0,0 +1,47 @@
+"""
+Test script for testing routes for all mini features .
+version 1.0
+Author - WaleedAhmed05
+"""
+import unittest
+from unittest.mock import patch
+import app
+
+class MiscRouteTests(unittest.TestCase):
+
+    def setUp(self):
+        app.app.testing = True
+        self.app = app.app.test_client()
+
+# Tests for Filter Jobs Routes #
+
+    @patch('controllers.filter_jobs_controller.FilterJobsController.get_filtered_jobs')
+    def test_get_filtered_jobs(self, mock_get_filtered_jobs):
+        # Mock the FilterJobsController.get_filtered_jobs() method
+        mock_get_filtered_jobs.return_value = [{'title': 'Job1 - Guardian of the Repositories'},
+                                               {'title': 'Job2 - Prince of Bugs'}]
+
+        # Simulate a GET request to the route
+        response = self.app.get('/api/filter_jobs/')
+
+        self.assertEqual(response.status_code, 200)
+
+        # Check if the response JSON matches the expected list of filtered jobs
+        self.assertEqual(response.json, [{'title': 'Job1 - Guardian of the Repositories'},
+                                         {'title': 'Job2 - Prince of Bugs'}])
+
+    @patch('controllers.filter_jobs_controller.FilterJobsController.get_filtered_jobs')
+    def test_get_filtered_jobs_error(self, mock_get_filtered_jobs):
+        # Mock the FilterJobsController.get_filtered_jobs() method
+        mock_get_filtered_jobs.side_effect = Exception('Some error occurred')
+
+        # Simulate a GET request to the route
+        response = self.app.get('/api/filter_jobs/')
+
+        self.assertEqual(response.status_code, 500)
+
+        # Check if the response JSON contains the error message
+        self.assertEqual(response.json, {'error': 'Some error occurred'})
+
+if __name__ == '__main__':
+    unittest.main()
