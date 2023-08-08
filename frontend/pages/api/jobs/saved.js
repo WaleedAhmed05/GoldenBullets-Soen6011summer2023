ac83929f77a885c8de049a498a9607cf5074da13
Sadman Shawmik <hello@sadmansh.com>
2023-08-08 15:20:15 -0400

    Add UI for job post bookmarks

---
+++ frontend/pages/api/jobs/saved.js
@@ -0,0 +1,28 @@
+export default async (req, res) => {
+	try {
+		// Get access token from authorization header
+		const authHeader = req.headers.authorization
+		if (!authHeader) {
+			return res.status(401).json({ message: 'Authorization header missing' })
+		}
+		if (req.method === 'GET') {
+			// Get saved jobs
+			const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/jobs/bookmark`, {
+				method: 'GET',
+				headers: {
+					Authorization: authHeader,
+					'Content-Type': 'application/json',
+				},
+			})
+			const data = await response.json()
+			if (response.ok) {
+				return res.status(200).json(data)
+			}
+		} else {
+			return res.status(405).json({ message: 'Method not allowed' })
+		}
+	} catch (error) {
+		console.error('Error accessing api/jobs', error)
+		return res.status(403).json({ message: 'User forbidden' })
+	}
+}
