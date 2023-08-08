f085b59b91ef48a1e953d695d3b5bdd12edb08a0
WaleedAhmed05 <waloo_waleed@yahoo.com>
2023-08-08 00:33:58 -0400

    more unit tests for browse_candidate_test.py file

--- backend/app/tests/browse_candidate_test.py
+++ backend/app/tests/browse_candidate_test.py
@@ -28,3 +28,20 @@ class TestAuthRoutes(unittest.TestCase):
         response = self.app.get('/api/browse_candidates/allcandidateskills')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json, ['Skill1', 'Skill2', 'Skill3'])
+
+    @patch('controllers.browse_candidates_controller.BrowseCandidatesController.get_all_candidates')
+    def test_get_all_candidates_error(self, mock_get_all_candidates):
+        # Simulate an exception in BrowseCandidatesController.get_all_candidates()
+        mock_get_all_candidates.side_effect = Exception('Some error occurred')
+        response = self.app.get('/api/browse_candidates/allcandidates')
+        self.assertEqual(response.status_code, 500)
+        self.assertEqual(response.json, {'error': 'Some error occurred'})
+
+    @patch('controllers.browse_candidates_controller.BrowseCandidatesController.get_all_candidateskills')
+    def test_get_all_skills_error(self, mock_get_all_candidateskills):
+        # Simulate an exception in BrowseCandidatesController.get_all_candidateskills()
+        mock_get_all_candidateskills.side_effect = Exception('Another error occurred')
+        response = self.app.get('/api/browse_candidates/allcandidateskills')
+        self.assertEqual(response.status_code, 500)
+        self.assertEqual(response.json, {'error': 'Another error occurred'})
+
