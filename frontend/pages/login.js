efd980eed959ecd9bc8f20c705b305f6bcf2884b
Nastaran-Naseri <49358045+Nastaran-Naseri@users.noreply.github.com>
2023-08-09 01:07:41 -0400

    update login.js frontend

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -22,17 +22,67 @@ const Login = () => {
 			<Header />
 			<main>
 				<div className={styles.login}>
+				{/*	<div className={styles.loginEmployer}>*/}
+				{/*		<h2>Want to hire?</h2>*/}
+				{/*		<p>Sign up as an employer to post jobs</p>*/}
+				{/*		<Link href={`${process.env.NEXT_PUBLIC_API_URL}/auth/login?type=employer`} className={styles.googleLogin}>Signup/Login as an employer</Link>*/}
+				{/*	</div>*/}
+				{/*	<div className={styles.loginCandidate}>*/}
+				{/*		<h2>Looking for a job?</h2>*/}
+				{/*		<p>Sign up as a candidate to apply for jobs</p>*/}
+				{/*		<Link href={`${process.env.NEXT_PUBLIC_API_URL}/auth/login?type=candidate`} className={styles.googleLogin}>Signup/Login as a candidate</Link>*/}
+				{/*	</div>*/}
+
+
+
+                <div className={styles.test}>
+					<div className="styles.hero-video__wrap">
+						<video autoPlay loop muted
+							   poster="https://www.flowd.co.uk/wp-content/themes/flowd/img/video/flowd.jpg" id="bgvid">
+							<source src="https://www.flowd.co.uk/wp-content/themes/flowd/img/video/flowd.webm"
+									type="video/webm"/>
+
+							<source src="https://www.flowd.co.uk/wp-content/themes/flowd/img/video/flowd.mp4"
+									type="video/mp4">
+							</source>
+							<source src="https://www.flowd.co.uk/wp-content/themes/flowd/img/video/flowd.ogg"
+										type="video/ogg"/>
+
+						</video>
+					</div>
+					<div className={styles.sheldonBg}>
+					</div>
+					<div className={styles.verticalLineonDiv}>
+
+					</div>
+					<div className={styles.textOnBg}>
 					<div className={styles.loginEmployer}>
-						<h2>Want to hire?</h2>
-						<p>Sign up as an employer to post jobs</p>
+						<h3>Want to hire?</h3>
+						{/*<p>Sign up as an employer to post jobs</p>*/}
 						<Link href={`${process.env.NEXT_PUBLIC_API_URL}/auth/login?type=employer`} className={styles.googleLogin}>Signup/Login as an employer</Link>
+						<div className={styles.vSlidesWrap}>
+							<div className={styles.vSlides}>
+								<h2 className={styles.vSlide} >Post job listings and reach qualified candidates</h2>
+							</div>
+						</div>
 					</div>
 					<div className={styles.loginCandidate}>
-						<h2>Looking for a job?</h2>
-						<p>Sign up as a candidate to apply for jobs</p>
+						<h3>Looking for a job?</h3>
+						{/*<p>Sign up as a candidate to apply for jobs</p>*/}
 						<Link href={`${process.env.NEXT_PUBLIC_API_URL}/auth/login?type=candidate`} className={styles.googleLogin}>Signup/Login as a candidate</Link>
+						<div className={styles.vSlidesWrap}>
+							<div className={styles.vSlides}>
+								<h2 className={styles.vSlide} >Apply to a wide range of job opportunities</h2>
+							</div>
+						</div>
 					</div>
-				</div>
+					</div>
+                  </div>
+
+	           </div>
+				{/*   <div className={styles.test2}>*/}
+				{/*	bjkzfnbf;lkbnxf;*/}
+				{/*</div>*/}
 			</main>
 		</>
 	)
