1021c08e2763d5876e159e05952bb8c4ae68efca
Sadman Shawmik <hello@sadmansh.com>
2023-08-09 14:18:06 -0400

    Add candidate browse, bookmark & invite to job UI

---
+++ frontend/pages/api/candidate/skills.js
@@ -0,0 +1,28 @@
+export default async (req, res) => {
+	try {
+		// Get access token from authorization header
+		const authHeader = req.headers.authorization
+		if (!authHeader) {
+			return res.status(401).json({ message: 'Authorization header missing' })
+		}
+		if (req.method === 'GET') {
+			// Send a GET request to fetch all skills
+			const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/browse_candidates/allcandidateskills`, {
+				method: 'GET',
+				headers: {
+					Authorization: authHeader,
+					'Content-Type': 'application/json',
+				},
+			})
+			const data = await response.json()
+			if (response.ok) {
+				return res.status(200).json(data)
+			}
+		} else {
+			return res.status(405).json({ message: 'Method not allowed' })
+		}
+	} catch (error) {
+		console.error('Error accessing api/candidate', error)
+		return res.status(403).json({ message: 'User forbidden' })
+	}
+}
--- frontend/pages/notifications.js
+++ frontend/pages/notifications.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import Link from 'next/link'
 import { useAppContext } from '@/context/AppContext'
 import Header from '@/components/layout/Header'
 import styles from '@/styles/Notifications.module.scss'
@@ -26,6 +27,21 @@ const Notifications = () => {
 		if (res.ok) {
 			// Sort notifications by created_at date
 			data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+
+			// Parse notifications to replace job id with job link
+			data.forEach(notification => {
+				if (notification.title === 'New job application invitation') {
+					// Get job id and title from notification body. Example body: You have been invited to apply to the job "#15 Senior UX/UI Designer"
+					const regex = /#(\d+)\s(.*)"/
+					const match = notification.body.match(regex)
+					if (!match) return
+					const jobId = match[1]
+					const jobTitle = match[2]
+					// Add link to job in notification body. Use next link
+					notification.jobId = jobId
+					notification.jobTitle = jobTitle
+				}
+			})
 			setNotifications(data)
 		}
 	}
@@ -70,7 +86,16 @@ const Notifications = () => {
 															{notification.title}
 														</h3>
 														<div className={styles.notificationContent}>
-															<p>{notification.body}</p>
+															<p>
+																{notification.title === 'New job application invitation' ? (
+																	/* Replace #<id> <title> in notification.body with link to job and title */
+																	<>
+																		You have been invited to apply to the job <Link href={`/jobs/${notification.jobId}`}>{notification.jobTitle}</Link>
+																	</>
+																) : 
+																	notification.body
+																}
+															</p>
 															<button type="button" onClick={() => markAsRead(notification.id)}>Mark as read</button>
 														</div>
 													</li>
