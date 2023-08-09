a93940246907aaccff8bcd8641feab8fb7003be9
dinaomidvartehrani <79149559+dinaomidvartehrani@users.noreply.github.com>
2023-08-09 16:31:19 -0500

    Update Test.py

--- frontend/test/Test.py
+++ frontend/test/Test.py
@@ -1,1 +1,20 @@
- k
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome(executable_path='C:\\Users\\Nastaran\\Downloads\\chromedriver_win32\\chromedriver.exe')
+driver.get("http://localhost:3000")
+
+time.sleep(3)
+driver.find_element_by_id("loginbtn").click()
+time.sleep(3)
+
+driver.find_element_by_link_text("Signup/Login as a candidate").click()
+
+driver.find_element_by_css_selector("input.whsOnd.zHQkBf").send_keys("saragary30@gmail.com")
+time.sleep(3)
+driver.find_element_by_xpath("//span[text()='Next']").click()
+# saragary30@gmail.com
+time.sleep(3)
+driver.find_element_by_css_selector("input.whsOnd.zHQkBf").send_keys("SG1234!ara")
+# time.sleep(2)
+# driver.find_element_by_xpath("//span[text()='Next']").click()
